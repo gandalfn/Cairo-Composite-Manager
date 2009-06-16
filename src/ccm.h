19d3eba44dde77852327c4a19266b9c1bc243fc3
Nicolas Bruguier <nicolas.bruguier@supersonicimagine.fr>
2009-06-16 19:38:47 +0200

    [cairo-compmgr] check for rectangle on free - check if rectangle is not null on rectangle free avoid a crash with shadow plugin

--- src/ccm.h
+++ src/ccm.h
@@ -150,8 +150,8 @@ gboolean        ccm_region_device_transform_invert (CCMRegion         *self,
 gboolean        ccm_region_point_in         (CCMRegion* self, int x, int y);
 gboolean        ccm_region_is_shaped        (CCMRegion* self);
 
-#define cairo_rectangles_free(c, nb) (g_slice_free1(sizeof(cairo_rectangle_t) * nb, c))
-#define x_rectangles_free(c, nb) (g_slice_free1(sizeof(XRectangle) * nb, c))
+#define cairo_rectangles_free(c, nb) ({if (c) g_slice_free1(sizeof(cairo_rectangle_t) * nb, c); })
+#define x_rectangles_free(c, nb) ({if (c) g_slice_free1(sizeof(XRectangle) * nb, c);})
 
 /******************************************************************************/
 
